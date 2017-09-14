10f936ac6dd54fdb40e44c2180c54842f4363eb1
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-14 21:51:39 +0700

    adding coding break document

--- app/index.js
+++ app/index.js
@@ -1,15 +1,6 @@
 // part 1
-// let wizard = { magical: true, power: 10 }
-// let magical = wizard.magical
-// let power = wizard.power
-// let { magical, power } = wizard
-// console.log(magical, power)
-
-// part 2
-let magical = true
-let power = 2
-let ranger = { magical: false, power: 9 }; // If not ';' -> Cannot read property 'magical' of undefined
-// { magical, power } = ranger; // Syntax Error because this defines a coding block
-// -->
-({magical, power} = ranger); // valid
-console.log(magical, power);
+let wizard = { magical: true, power: 10 }
+// let m = wizard.magical
+// let p = wizard.power
+let { magical, power } = wizard
+console.log(magical, power)
