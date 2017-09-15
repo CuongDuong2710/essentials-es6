f983c261298122d13cd9a1443ec5ac47369c8633
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-15 13:34:56 +0700

    exporting values

---
+++ lecture/06_modules/exporting_values/students.js
@@ -0,0 +1,4 @@
+// modules in javascript refer to reusable pieces of code within our application or system
+// export can send functions, objects or primitive variables from a module to other
+export const students = ["Harry", "Ron", "Hermione"]
+export const total = 505
