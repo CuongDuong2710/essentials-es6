2accc1ea731b036fdf1831b287bf91d678dc58f3
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-15 11:32:22 +0700

    exercies with map method

--- lecture/05_functions_methods/25_map_method/index.js
+++ lecture/05_functions_methods/25_map_method/index.js
@@ -32,3 +32,11 @@ console.log(points)
 points = points.map(element => element + 1)
     
 console.log(points)
+
+// exercise
+let lengths = [3, 7, 5];
+let multiple = 8;
+
+const scale = (multiple) => {
+  return lengths.map(element => element * multiple);
+};
