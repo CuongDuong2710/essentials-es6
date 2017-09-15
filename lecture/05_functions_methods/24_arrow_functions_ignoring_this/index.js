079e0d69d4145cd4c466164fdf86ce9aa7623da5
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-15 11:25:23 +0700

    helping with map method

---
+++ lecture/05_functions_methods/24_arrow_functions_ignoring_this/index.js
@@ -0,0 +1,36 @@
+// remove 'babel->preset->es2015 in package.json
+// global all pars
+this.a = 25
+
+// part 1
+// A print function creates its own (this.a) as seperate from the global base around.
+// let print = function() {
+//   console.log('this.a', this.a)
+// }
+
+// print() // Uncaught TypeError: Cannot read property 'a' of undefined
+
+// part 2
+// let print = function() {
+//   this.a = 50;
+//   console.log('this.a', this.a);
+// }
+
+// print(); // Uncaught TypeError: Cannot read property 'a' of undefined
+
+// part 3
+let arrowPrint = () => {
+  console.log('this.a in arrowPrint', this.a) // this.a = 25
+}
+
+arrowPrint()
+
+// part 4
+// function outerFunction() {
+//   this.a = 25;
+//   let innerFunction = () => {
+//     console.log(this); // does not create its own this
+//   }
+// }
+
+// outerFunction() // Uncaught TypeError: Cannot read property 'a' of undefined
