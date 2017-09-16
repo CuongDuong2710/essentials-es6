1fdf583e5982a31d715fad3e620e1bbd8f64aa0a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-16 14:10:12 +0700

    promise function to get weather api

--- app/index.js
+++ app/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom' //this handles actually rendering react elements
 
+const api_key = '6fab9e81b0b0ba2f60f1befb0b507074';
+
 class App extends Component { //it will inherit a bunch of useful methods from 'Component' class
+  // 'props' checks for data and paired components that allow constructor to manipulate data
+  constructor(props) {
+    super(props)
+    // all component contain the relevant data in an object called 'state'
+    this.state = {
+      city: 'San Francisco',
+      description: ''
+    }
+  }
+
+  // This allow us to run code when the app fully renders on the screen
+  componentDidMount() {
+    this.grabWeather(this.state.city)
+  }
+
+  grabWeather(city) {
+    // 'fetch' to use handle setting a web request the API for your application
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({description: json.weather[0].description})
+      })
+  }
+
   render() {
     return (
-      <div>React JS and JSX in action!</div>
+      <div>
+        <h1>Weather report for {this.state.city}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
     )
   }
 }
